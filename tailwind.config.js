d7767024e6613cdf0252b770273a8d7d1aed34f3
masud563 <sydmasud563@gmail.com>
2021-12-31 15:57:43 +0600

    home layout is set

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
